d65a5846f3943a93b474ebb95c20284caf24fe0d
rodrigosebben <rsebben@restinga.ifrs.edu.br>
2016-04-15 14:55:47 -0300

    Sistema inicial
    Criação de sistema inicial

---
+++ Fontes/Contabil.dpr
@@ -0,0 +1,14 @@
+program Contabil;
+
+uses
+  Forms,
+  UnitFormPrincipal in 'UnitFormPrincipal.pas' {FormPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFormPrincipal, FormPrincipal);
+  Application.Run;
+end.
